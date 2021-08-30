a6b0f75949502f3bc545d2595ed0ef45a28049f9
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-08-30 17:08:15 +0530

    app.js initial ritual done

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
-  return <> </>;
-}
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <div>
+          <Switch>
+            {/* <Route path="/" component={login} exact></Route> */}
+            <Route path="/home" component={LogSign} exact></Route>
+            <Route path="/account" component={AccountPage} exact></Route>
+            {/* <Route path="/doctorHome" component={HomePage} exact></Route> */}
+            {/* <Route path="/NurseHome" component={HomePage} exact></Route> */}
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </>
+  );
+};
 
 export default App;
