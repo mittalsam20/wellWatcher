b3d0addcc3413bf94d16e424e476e3a2b19650cc
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-08-30 16:36:22 +0530

    decluttering

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.router();
-cont bodyparser = require("body-parser");
+// cont bodyparser = require("body-parser");
 const bcrypt = require("body-parser");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
 
 router.use(cors());
 router.use(bodyParser.urlencoded({ extended: true }));
+module.exports = router;
